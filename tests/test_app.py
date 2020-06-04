e84a7c9123859ad2202a68a6263b2311445a7481
Robert Owen <robert.owen@softwire.com>
2020-06-04 11:58:59 +0100

    exercise 2 solution

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,6 @@
 """Integration tests for app.py"""
+import json
+
 import pytest
 
 from bank_api.app import app
@@ -10,9 +12,19 @@ def client():
     return app.test_client()
 
 
-def test_account_creation(client):
-    # Use the client to make requests e.g.:
-    # client.post(...)
-    # client.get(...)
-    # https://flask.palletsprojects.com/en/1.1.x/testing/
-    pass
+def test_create_account(client):
+    """I should be able to create, then query an account"""
+    response_creation = client.post('/accounts/test')
+    response_query = client.get('/accounts/test')
+
+    assert response_creation.status_code == 200
+    assert response_query.status_code == 200
+
+    data = json.loads(response_query.data)
+    assert data['name'] == 'test'
+
+
+def test_get_invalid_account_fails_with_404(client):
+    """Querying a non-existant account returns 404 (Not Found)"""
+    response_query = client.get('/accounts/nothere')
+    assert response_query.status_code == 404
